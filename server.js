ae6c3adefefed2f24ee86abd4717d3b9e88aa3ac
Priyanshu Singh <priyanshu2002krsingh@gmail.com>
2025-06-12 01:45:12 +0530

    shift mongodb to online server

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const db = require("./db");
-
+require('dotenv').config();
 
 const app = express();
 
 const bodyParser = require("body-parser");
 app.use(bodyParser.json()); //req.body
+const PORT = process.env.PORT || 3000; 
 
-let PORT = 3000;
+//let PORT = 3000;
 
 app.get("/", (req, res) => {
   res.send("Welcome to our Hotel");
@@ -19,6 +20,8 @@ const menuItemRoutes = require('./routes/menuItemRoutes');
 //Use the routes
 app.use('/person', personRoutes);
 app.use('/menu', menuItemRoutes);
+
+
 
 app.listen(PORT, () => {
   console.log("App is listening on port");
